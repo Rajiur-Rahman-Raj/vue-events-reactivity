ce19adcc78d0c135e6ba0f8a58fe287d9d4a56c4
Rajiur Rahman <rajiurrahmanraj2021@gmail.com>
2023-08-03 08:04:15 +0600

    event handling & modifires

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,62 @@
 <script setup>
-    import { ref } from 'vue';
-    const defaultThumb = ref('https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60')
-    const images = [
-        {
-            thumb: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
-            full: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
-        },
-        {
-            thumb: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=300&q=60",
-            full: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
-        },
-        {
-            thumb: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
-            full: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
-        },
-        {
-            thumb: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
-            full: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
-        }
-    ]
+    import {
+        ref
+    } from 'vue';
 
-    function changeImage(image){
-        defaultThumb.value = image;
+    const output = ref('Hellow vue 3')
+    const keyPress = ref(0)
+
+    function spaceCount(){
+        spaceCount = keyPress.value++
+        output.value = `You pressed space ${spaceCount} times`
+    }
+
+    function pressWhat(event){
+        // output.value = `You pressed ${event.key}`
+        output.value = `You pressed ${event.target.value}`
+        console.log(event)
+    }
+
+    function ctrlRightClicked(){
+        output.value = `you clicked ctrl+right`
+    }
+
+    function divClicked(){
+        output.value = `div clicked`
+        alert('div clicked')
+    }
+
+    function buttonClick(){
+        output.value = `button clicked`
+        alert('button clicked')
     }
 </script>
 
 <template>
     <section class="container mx-auto flex items-center flex-col">
-        <h1 class="text-center text-2xl py-10">Events & Reactivity</h1>
-        <div class="">
-            <div class="flex space-x-2">
-                <img class="cursor-pointer" v-for="image in images" :key="image.thumb" :src="image.thumb" alt="image.thumb" @click="changeImage(image.full)">
+        <h1 class="text-center text-2xl py-10">Events &amp; Reactivity in Vue.js</h1>
+        <h2 class="mt-10">{{ output }}</h2>
+        <form action="https://google.com/search">
+            <div class="container mx-auto flex space-x-5 justify-center m-5">
+                <input name="q" type="text" class="border border-gray-500 bg-white p-5" placeholder="Press Space" @keypress.space="spaceCount()">
             </div>
-            <div class="mt-10">
-                <img class="cursor-pointer w-full" :src="defaultThumb" alt="">
+
+            <div class="container mx-auto flex space-x-5 justify-center m-5">
+                <input name="text" type="text" class="border border-gray-500 bg-white p-5" placeholder="Type Something" @keyup="pressWhat($event)">
             </div>
+
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click.prevent>
+                 Button 
+            </button>
+
+            <button class="bg-blue-500 ml-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click.ctrl="ctrlRightClicked()">
+                 Ctrol + Right Click 
+            </button>
+        </form>
+        <div class="bg-gray-200 w-[500px] h-80 mt-5 flex items-center justify-center" @click="divClicked()">
+            <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" @click.stop="buttonClick()"> 
+                Button 
+            </button>
         </div>
     </section>
 </template>
